0483428e35a6d03cf93c9682877a8249df76cd3a
ammrshmbng <ammarmare01@gmail.com>
2022-06-09 21:18:16 +0700

    tambah

--- controllers/game.js
+++ controllers/game.js
@@ -2,11 +2,52 @@ const db = require('../models');
 
 const game = async (req,res)=>{
     try {
+
+        const {pilihanP1,pilihanP2} = req.body;
+
         const  dataRoom = await db.user_game_room.findOne({
-            where: {id: req.body.id}
-            
+            where: {id: req.body.id}            
             
         });
+
+        // check data room jika tidak ada
+        if(!dataRoom){
+            res.json({message: "room belum di buat"})
+        }
+
+        // main game
+        function getPilihanComputer(){
+            let comp = Math.random();
+            if(comp < 0.34) return 'gunting';
+            if(comp >= 0.34 && comp < 0.67) return 'batu';
+            return 'kertas'
+            
+        }
+        
+        function getHasil(comp,player){
+           
+              if(player == comp) return "DRAW!";
+              if(player == 'gunting') return (comp == 'kertas') ? 'YOU WIN!' : 'YOU LOSE!';
+              if(player == 'kertas') return (comp == 'gunting') ? 'YOU LOSE!' : 'YOU WIN!';
+              if(player == 'batu') return (comp == 'gunting') ? 'YOU WIN!' : 'YOU LOSE!';
+                                    return 'salah input';
+              
+        }
+        
+        // menentukan pilihan 
+        const pilihanCom = getPilihanComputer();
+        let hasil =''
+
+
+        if(pilihanP1){
+            hasil = getHasil(pilihanCom,pilihanP1);
+            res.json(hasil);
+        }else if(pilihanP2){
+            hasil = getHasil(pilihanCom,pilihanP2);
+            res.json(hasil);
+        }
+        
+    
         
 
     } catch (error) {
--- routes/game.js
+++ routes/game.js
@@ -2,7 +2,8 @@ const router = require('express').Router();
 
 // import controller
 const {game} =require('../controllers/game');
+const {jwtAuthorization} = require('../middlewares/jwt');
 
-router.post('/',game);
+router.post('/',jwtAuthorization,game);
 
 module.exports = router;
